68cf92f12aa98dd9c3e55c3abf02fca6027fe1f3
Rudy Gall <rudyegall@gmail.com>
2019-01-16 11:52:10 -0500

    Second Commit

--- jan19Lab4/Program.cs
+++ jan19Lab4/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace jan19Lab4
@@ -11,13 +12,14 @@ namespace jan19Lab4
         static void Main(string[] args)
         {
             bool runP = true;
-            Console.WriteLine("Squares and Cubes");
+            string firstName = nameValid();
+            Console.Clear();
+            Console.WriteLine("\nHello {0}, Let us display a Squares and Cubes Table of Powers", firstName);
             while (runP == true)
             {
-                Console.WriteLine("\nEnter an integer");
-                //int userNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nPlease enter an integer");
                 int.TryParse(Console.ReadLine(), out int userNum);
-                Console.WriteLine("Number       Squared       Cubed");
+                Console.WriteLine("\nNumber       Squared       Cubed");
                 Console.WriteLine("======       ======        ======");
                 for (int i = 1; i <= userNum; i++)
                 {
@@ -60,6 +62,27 @@ namespace jan19Lab4
         {
             int cubeNum = userNum * userNum * userNum;
             return cubeNum;
+        }
+        public static string nameValid()
+        {
+            bool responseValid = true;
+            string firstName = "";
+            while (responseValid)
+            {
+                Console.WriteLine("Please enter your first name");
+                firstName = Console.ReadLine();
+
+                if (!Regex.IsMatch(firstName, @"^[A-Z]+[A-z]{2,30}$"))
+                {
+                    Console.WriteLine("I'm sorry, this is not a valid name");
+                }
+                else
+                {
+                    Console.WriteLine("Name is valid");
+                    break;
+                }
+            }
+            return firstName;
         }
     }
 }
